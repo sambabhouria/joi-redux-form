a045c436d6de1d576e64cb97706ccb04669ba021
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-10-30 16:28:57 +0100

    email passsword added

--- src/form/form.js
+++ src/form/form.js
@@ -4,22 +4,54 @@ import './form.css';
 
 const Joi = require("@hapi/joi");
 
+
+// schema a valider par etape
 const postSchema = Joi.object().keys({
     title: Joi.string()
       .min(10)
       .max(140)
       .required(),
-    authorName: Joi.string().required(),
+    //a required string must contain only alphanumeric characters
+     //at least 3 characters long but no more than 30
+     //must be accompanied by birthyear
+    authorName: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+    // an integer between 1900 and 2013
+    birthyear: Joi.number()
+    .integer()
+    .min(1900)
+    .max(2013),
+    // a valid email address string must have two domain parts e.g. example.com
+    //TLD must be .com or .net , .fr
+    email:Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr'] } }),
     slug: Joi.string()
       .regex(/^[a-z0-9-]+$/)
       .required(),
     published: Joi.date()
       .iso()
       .required(),
-    content: Joi.string()
-});
+    content: Joi.string(),
+    password: Joi.string()
+    .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    repeatPassword: Joi.ref('password'),
+    accessToken: [
+      Joi.string(),
+      Joi.number()
+  ],
+})
+.with('authorName', 'birthyear')
+.xor('password', 'accessToken')
+.with('password', 'repeatPassword')
+;
 
+// fonction qui  prend en paramettre les inputes et retour l'erreur
+   // si le schema n'est pas valide ou un objet vide
 const validate = values => {
+  console.log("values===", values)
   const { error } = postSchema.validate(values);
   console.log("errror", { ...error })
   if (error) {
@@ -57,6 +89,8 @@ class Form extends Component {
           component={renderField}
           label="Author Name"
         />
+        <Field name="birthyear" type="birthyear" component={renderField} label="Birth Year" />
+        <Field name="email" type="text" component={renderField} label="Email" />
         <Field name="slug" type="text" component={renderField} label="Slug" />
         <Field
           name="published"
@@ -70,6 +104,10 @@ class Form extends Component {
           component={renderField}
           label="Content"
         />
+        <Field name="password" type="text" component={renderField} label="Password" />
+        <Field name="repeatPassword" type="text" component={renderField} label="Repeat Password" />
+        <Field name="accessToken" type="text" component={renderField} label="Access Token" />
+
         <div>
           <button type="submit" disabled={submitting}>
             Submit
